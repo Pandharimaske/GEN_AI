535deafe91fddc0bdbdb072cb33c1047751d8df6
Pandhari <pandhari2527@gmail.com>
2025-05-16 00:16:44 +0530

    stated med proj

--- FAST-API/main.py
+++ FAST-API/main.py
@@ -1,14 +1,29 @@
 from fastapi import FastAPI
+import json
 
 # Create an instance of the FastAPI application
 app = FastAPI()
 
+
+def load_data():
+    with open("patients.json" , 'r') as f:
+        data = json.load(f)
+
+    return data
+
 # Root route: returns a simple Hello World message
 @app.get('/')
 def hello():
-    return {"message": "Hello World!"}
+    return {"message": "Patient Management System API"}
 
 # About route: returns info about CampusX
 @app.get('/about')
 def about():
-    return {"message": "CampusX is an educational platform"}
+    return {"message": "A fully functional API to manage your patient records"}
+
+
+@app.get('/view')
+def view():
+    data = load_data()
+
+    return data
